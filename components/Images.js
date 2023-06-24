e6da6ca1a55b3cd52f80a508a5599a23b785f444
medaka <36759068+medaka0213@users.noreply.github.com>
2023-06-24 19:43:58 +0900

    react-vrw 更新

--- components/Images.js
+++ components/Images.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import SaveAltIcon from "@mui/icons-material/SaveAlt";
 
 import { ImagePreview, ImageDownloadButton } from "react-vrw";
 

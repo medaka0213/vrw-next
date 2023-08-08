41d2409924c318a5c47e3b1f965f53d9900e5e19
medaka <36759068+medaka0213@users.noreply.github.com>
2023-08-09 00:43:05 +0900

    getInitialProps にしてみる

--- pages/q/[type]/i/[pk].tsx
+++ pages/q/[type]/i/[pk].tsx
@@ -1,8 +1,7 @@
-import { InferGetServerSidePropsType } from "next";
 import React from "react";
 
 import MissionPage from "@/components/MissionPage";
-import LinearProgress from "@mui/material/LinearProgress";
+import Error404 from "@/components/Error404";
 
 import OgpHead from "@/components/OgpHead";
 import { MissionDetail } from "@/lib/models";
@@ -12,49 +11,12 @@ import {
   fetGetReference,
 } from "@/lib/client";
 
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: "blocking",
-  };
-}
-
-export const getStaticProps = async (context: any) => {
-  const { params } = context;
-
-  const type = params.pk.split("_")[0];
-  if (["mission", "launch", "event"].indexOf(type) < 0) {
-    return { notFound: true };
-  }
-  // アイテム
-  const item = await fetGetSingleItem({ pk: params.pk, type });
-  if (!item) {
-    return { notFound: true };
-  }
-  // 関連アイテム
-  const relation = await fetGetRelation({ pk: params.pk, type });
-  const rocket = relation.rocket ? relation.rocket[0] : null;
-  const slide = relation.slide ? relation.slide[0] : null;
-  const meetup = relation.meetup || [];
-  // 参照アイテム
-  const reference = await fetGetReference({ pk: params.pk, type });
-  const countdown = reference.countdown ? reference.countdown[0] : null;
-  return {
-    props: {
-      item: item.data(),
-      rocket: rocket ? rocket.data() : null,
-      slide: slide ? slide.data() : null,
-      meetup: meetup?.map((item: any) => item.data()) || [],
-      countdown: countdown ? countdown.data() : null,
-      loaded: true,
-    },
-    revalidate: 600,
-  };
-};
-
-const App = (_data: InferGetServerSidePropsType<typeof getStaticProps>) => {
-  let data = new MissionDetail(_data);
-  return _data.loaded ? (
+const App = (props = {}: any) => {
+  console.log("App", props);
+  let data = new MissionDetail(props);
+  return props.notFound ? (
+    <Error404 />
+  ) : (
     <>
       <OgpHead
         title={"ミッション情報 : " + data.title()}
@@ -62,9 +24,37 @@ const App = (_data: InferGetServerSidePropsType<typeof getStaticProps>) => {
       />
       <MissionPage data={data} />
     </>
-  ) : (
-    <LinearProgress />
   );
+};
+
+App.getInitialProps = async (context: any) => {
+  console.log("getInitialProps", context);
+  const { query } = context;
+
+  const type = query.pk.split("_")[0];
+  if (["mission", "launch", "event"].indexOf(type) < 0) {
+    return { notFound: true };
+  }
+  // アイテム
+  const item = await fetGetSingleItem({ pk: query.pk, type });
+  if (!item) {
+    return { notFound: true };
+  }
+  // 関連アイテム
+  const relation = await fetGetRelation({ pk: query.pk, type });
+  const rocket = relation.rocket ? relation.rocket[0] : null;
+  const slide = relation.slide ? relation.slide[0] : null;
+  const meetup = relation.meetup || [];
+  // 参照アイテム
+  const reference = await fetGetReference({ pk: query.pk, type });
+  const countdown = reference.countdown ? reference.countdown[0] : null;
+  return {
+    item: item.data(),
+    rocket: rocket ? rocket.data() : null,
+    slide: slide ? slide.data() : null,
+    meetup: meetup?.map((item: any) => item.data()) || [],
+    countdown: countdown ? countdown.data() : null,
+  };
 };
 
 export default App;
--- pages/q/meetup/i/[pk].tsx
+++ pages/q/meetup/i/[pk].tsx
@@ -1,8 +1,7 @@
-import { InferGetServerSidePropsType } from "next";
 import React from "react";
 
 import MeetupPage from "@/components/MeetupPage";
-import LinearProgress from "@mui/material/LinearProgress";
+import Error404 from "@/components/Error404";
 
 import { MeetupDetail } from "@/lib/models";
 import {
@@ -12,43 +11,12 @@ import {
 } from "@/lib/client";
 import OgpHead from "@/components/OgpHead";
 
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: "blocking",
-  };
-}
-
-export const getStaticProps = async (context: any) => {
-  const { params } = context;
-
-  const type = params.pk.split("_")[0];
-  // アイテム
-  const item = await fetGetSingleItem({ pk: params.pk, type });
-  if (!item) {
-    return { notFound: true };
-  }
-  // 関連アイテム
-  const relation = await fetGetRelation({ pk: params.pk, type });
-  const image = relation.image || [];
-  // 参照アイテム
-  const reference = await fetGetReference({ pk: params.pk, type });
-  const launch = reference.launch ? reference.launch[0] : null;
-  const event = reference.event ? reference.event[0] : null;
-  return {
-    props: {
-      item: item.data(),
-      mission: launch ? launch.data() : event ? event.data() : null,
-      poster: image?.map((item: any) => item.data()) || [],
-      loaded: true,
-    },
-    revalidate: 600,
-  };
-};
-
-const App = (_data: InferGetServerSidePropsType<typeof getStaticProps>) => {
-  let data = new MeetupDetail(_data);
-  return _data.loaded ? (
+const App = (props: any) => {
+  console.log("App", props);
+  let data = new MeetupDetail(props);
+  return props.notFound ? (
+    <Error404 />
+  ) : (
     <>
       <OgpHead
         title={"集会情報 : " + data.title()}
@@ -56,9 +24,31 @@ const App = (_data: InferGetServerSidePropsType<typeof getStaticProps>) => {
       />
       <MeetupPage data={data} />
     </>
-  ) : (
-    <LinearProgress />
   );
+};
+
+App.getInitialProps = async (context: any) => {
+  const { query } = context;
+
+  const type = query.pk.split("_")[0];
+  // アイテム
+  const item = await fetGetSingleItem({ pk: query.pk, type });
+  if (!item) {
+    return { notFound: true };
+  }
+  // 関連アイテム
+  const relation = await fetGetRelation({ pk: query.pk, type });
+  const image = relation.image || [];
+  // 参照アイテム
+  const reference = await fetGetReference({ pk: query.pk, type });
+  const launch = reference.launch ? reference.launch[0] : null;
+  const event = reference.event ? reference.event[0] : null;
+  return {
+    item: item.data(),
+    mission: launch ? launch.data() : event ? event.data() : null,
+    poster: image?.map((item: any) => item.data()) || [],
+    loaded: true,
+  };
 };
 
 export default App;
